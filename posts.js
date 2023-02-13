9175a3e5a99bb360955d3c4db7aa0eb7a0f0301f
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-02-13 14:16:17 -0800

    Add files via upload
    Remade many stuff

---
+++ posts.js
@@ -0,0 +1,14 @@
+///id = YYYYMMDDX X = Number of article that day
+//Always add to the top
+var items = [
+    {
+        id: 202302131,
+        title: "Boise Gems Begin With a Bang",
+        date: "Monday, February 13th, 2023",
+        author: "Chris Price",
+        text: "A new Drum and Bugle Corps based in Boise, Idaho has selected Chris Price to compose and arrange their first show, Esto Perpetua. The new Drum Corps brings opportunities to nearby students, letting their talent excel.",
+        img: "/media/post01.jpg",
+        type: "music news",
+        link: "/posts/202302131.html"
+    }
+]
