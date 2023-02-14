edd4a0e70ea54d68772ccc82c2fe2a9e46ddaf0b
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-02-13 16:26:09 -0800

    Add files via upload

--- posts.js
+++ posts.js
@@ -2,6 +2,16 @@
 //Always add to the top
 var items = [
     {
+        id: 202302132,
+        title: "Intro to Synthesizers",
+        date: "Monday, February 13th, 2023",
+        author: "Chris Price",
+        text: "A basic overview on how synthesizers work. Lots of concepts relating to math and physics will be covered in this article. Bit depth, sample rate, and other terms will become familiar to you, and you'll know how to tell if a pair of headphones is good or not.",
+        img: "/media/202302132/thumb.png",
+        type: "cs music",
+        link: "/posts/202302132.html"
+    },
+    {
         id: 202302131,
         title: "Boise Gems Begin With a Bang",
         date: "Monday, February 13th, 2023",
