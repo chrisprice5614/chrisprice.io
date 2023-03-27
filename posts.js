0719d1c1b5830311875993f5360ee966a5c49c22
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-03-27 16:24:04 -0700

    Add files via upload

--- posts.js
+++ posts.js
@@ -2,53 +2,13 @@
 //Always add to the top
 var items = [
     {
-        id: 202302143,
-        title: "Making Simplex Noise, Simple",
+        id: 1,
+        title: "General Conference Spring 2023",
         date: "Tuesday, February 14th, 2023",
-        author: "Chris Price",
-        text: "Simplex noise is a type of noise function that is commonly used in computer graphics and other fields to create random patterns with certain desirable properties. It involves dividing the space into regular grid cells and interpolating between the values of random vectors at the corner points.",
-        img: "/media/202302143/thumb.png",
-        type: "cs",
-        link: "/posts/202302143.html"
+        author: "",
+        text: "Join us as we watch the 2023 Spring general conference. On Saturday we will be meeting at the Hopkins, then on Sunday we will meet at the Gibby's. Hear wonderful talks, be around amazing people, and enjoy tasty food. See you there!",
+        img: "/media/post1/01.jfif",
+        type: "EVENTS",
+        link: "/posts/general_conference_spring2023.html"
     },
-    {
-        id: 202302142,
-        title: "If You Hear This Song, Someone Dies",
-        date: "Tuesday, February 14th, 2023",
-        author: "Chris Price",
-        text: "This eerie chant has been used for many years to represent death. Originally written as an old orthodox plainchant, this snippet of music is used by musicians everywhere to signify death. It's played more often than one would think.",
-        img: "/media/202302142/thumb.jpg",
-        type: "music",
-        link: "/posts/202302142.html"
-    },
-    {
-        id: 202302141,
-        title: "ASCII 10 or ASCII 13?",
-        date: "Tuesday, February 14th, 2023",
-        author: "Chris Price",
-        text: "When messing around with text files, you may have noticed that a pesky ASCII 10 or ASCII 13 character appears. What exactly are these characters and what do they mean? Is there really a difference between the two? Definitely, let's see what the difference is.",
-        img: "/media/202302141/thumb.jpg",
-        type: "cs",
-        link: "/posts/202302141.html"
-    },
-    {
-        id: 202302132,
-        title: "Intro to Synthesizers",
-        date: "Monday, February 13th, 2023",
-        author: "Chris Price",
-        text: "A basic overview on how synthesizers work. Lots of concepts relating to math and physics will be covered in this article. Bit depth, sample rate, and other terms will become familiar to you, and you'll know how to tell if a pair of headphones is good or not.",
-        img: "/media/202302132/thumb.png",
-        type: "cs music",
-        link: "/posts/202302132.html"
-    },
-    {
-        id: 202302131,
-        title: "Boise Gems Begin With a Bang",
-        date: "Monday, February 13th, 2023",
-        author: "Chris Price",
-        text: "A new Drum and Bugle Corps based in Boise, Idaho has selected Chris Price to compose and arrange their first show, Esto Perpetua. The new Drum Corps brings opportunities to nearby students, letting their talent excel.",
-        img: "/media/post01.jpg",
-        type: "music news",
-        link: "/posts/202302131.html"
-    }
 ]
